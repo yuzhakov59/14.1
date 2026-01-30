8a26d05b390f573d591b3671247170317ba33fb1
yuzhakov <yuzhakov.a.a@mail.ru>
2026-01-30 20:59:04 +0500

    14.2 готовый

--- src/category.py
+++ src/category.py
@@ -3,14 +3,32 @@ from src.product import Product
 class Category:
     name: str
     description: str
-    products: list
+    __products: list
     categories_count = 0
-    products_count = 0
+    product_count = 0
 
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.categories_count += 1
-        Category.products_count += len(products) if products else 0
+        Category.product_count += len(products) if products else 0
 
+    def add_product(self, product):
+        """Добавляет продукт в категорию"""
+        if isinstance(product, Product):
+            self.__products.append(product)
+        else:
+            raise ValueError("Можно добавлять только объекты класса Product")
+
+    @property
+    def products(self):
+        str_product = ""
+        for product in self.__products:
+            str_product += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return str_product
+
+    @products.setter
+    def products(self, products: Product):
+        self.__products.append(products)
+        Category.products_count += 1
--- src/product.py
+++ src/product.py
@@ -7,5 +7,26 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_dict: dict):
+        """Метод для создания объекта класса из параметров, указанных в виде словаря"""
+
+        name = product_dict.get('name', '')
+        description = product_dict.get('description', '')
+        price = product_dict.get('price', 0.0)
+        quantity = product_dict.get('quantity', 0)
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        self.__price = new_price
