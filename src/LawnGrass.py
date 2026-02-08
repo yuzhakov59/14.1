aff66e89a7516cd0d43a74f7658ffcb77179f42e
nout <yuzhakov.a.a@mail.ru>
2026-02-08 12:21:07 +0500

    16.1 исправления 3

---
+++ src/LawnGrass.py
@@ -0,0 +1,14 @@
+from src.product import Product
+
+
+class LawnGrass(Product):
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
+    def __add__(self, other):
+        if not isinstance(other, Product):
+            raise ValueError('TypeError')
+        return self.price + other.price
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 
 from src.product import Product
 from src.category import Category
+from src.Smartphone import Smartphone
+from src.LawnGrass import LawnGrass
 
 
 @pytest.fixture
@@ -34,5 +36,24 @@ def ovo_product():
 
 
 @pytest.fixture
-def app_product():
-    return Product('яблоко', 'статус яблока', 55, 5)
+def smartphone1():
+    return Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера",
+                      180000.0, 5, 95.5, "S23 Ultra", 256, "Серый")
+
+
+@pytest.fixture
+def smartphone2():
+    return Smartphone("Iphone 15", "512GB, Gray space", 210000.0,
+                      8, 98.2, "15", 512, "Gray space")
+
+
+@pytest.fixture
+def grass1():
+    return LawnGrass("Газонная трава", "Элитная трава для газона", 500.0,
+                     20, "Россия", "7 дней", "Зеленый")
+
+
+@pytest.fixture
+def grass2():
+    return LawnGrass("Газонная трава 2", "Выносливая трава", 450.0,
+                     15, "США", "5 дней", "Темно-зеленый")
