fba38a41a74d6c2a0e9e4b83631b0b78a0f54d87
yuzhakov <yuzhakov.a.a@mail.ru>
2026-02-09 21:35:06 +0500

    16.2 готовый_1.2 с тестами

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,3 +1,6 @@
+import pytest
+from src.product import Product
+
 def test_product_init(ovo_product):
     assert ovo_product.name == 'картошка'
     assert ovo_product.description == 'статус картошка'
@@ -10,3 +13,22 @@ def test_product_str(ovo_product):
 def test_product_add(ovo_product, app_product):
     sum_prod = ovo_product + app_product
     assert sum_prod == 450
+
+def test_typeerror(smartphone1, smartphone2, grass1):
+
+    expected_total_cost = (smartphone1.price * smartphone1.quantity) + (smartphone2.price * smartphone2.quantity)
+    assert expected_total_cost == 2580000.0
+
+
+    # Проверяем, что сложение с другим типом объекта вызывает TypeError
+    with pytest.raises(TypeError):
+        smartphone1 + grass1
+
+def test_dict_new_product():
+    new_prod = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5})
+    assert new_prod.name == "Samsung Galaxy S23 Ultra"
+    assert new_prod.description == "256GB, Серый цвет, 200MP камера"
+    assert new_prod.price == 180000.0
+    assert new_prod.quantity == 5
